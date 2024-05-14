018d5d6367c739b700264fde2f9fc361d6495544
Munchkin17 <tarrynwallie17@gmail.com>
2024-05-14 15:27:38 +0200

    skills added to the skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,8 +13,8 @@ import pythonLogo from "../assets/img/python_logo.png";
 import reactLogo from "../assets/img/react_logo.png";
 import jsLogo from "../assets/img/js_logo.png";
 import wordpressLogo from "../assets/img/js_logo.png";
-import adobeIllusLogo from "../assets/img/js_logo.png";
-import photoshopLogo from "../assets/img/js_logo.png";
+import adobeIllusLogo from "../assets/img/adobe illustrator logo.png";
+import photoshopLogo from "../assets/img/adobe-photoshop-logo.png";
 //import colorSharp from "../assets/img/color-sharp.png";
 
 
@@ -80,14 +80,14 @@ export const Skills = () => {
                                 <div className="item">
                                 <img src={csharpLogo} alt="C# logo" />
                                     <h3>C#</h3>
-                                    <img src={wordpressLogo} alt="Wordpress logo" />
-                                    <h3>Wordpress</h3>
+                                    <img src={"https://th.bing.com/th/id/R.7cab80917990eac1c026ad65766f75eb?rik=UQZMnibMe5Z9Vw&riu=http%3a%2f%2fareteit.com%2fwp-content%2fuploads%2f2014%2f12%2fWordpress_logo_8.png&ehk=O4jA0BXTCzM8FjsJcMOmQ51LNT8uoQgxImTdb9q4FDg%3d&risl=&pid=ImgRaw&r=0"} alt="Wordpress logo" style={{width: "100px", marginTop: "50px"}} />
+                                    <h3 style={{marginTop:"50px"}}>Wordpress</h3>
                                 </div>
                                 <div className="item">
-                                <img src={adobeIllusLogo} alt="Adobe Illustrator logo" />
-                                    <h3>Adobe Illustrator</h3>
-                                    <img src={photoshopLogo} alt="Photoshop logo" />
-                                    <h3>Adobe Photoshop</h3>
+                                <img src={adobeIllusLogo} alt="Adobe Illustrator logo" style={{width: "130px", marginTop: "65px"}}/>
+                                    <h3 style={{marginTop:"55px"}}>Adobe Illustrator</h3>
+                                    <img src={photoshopLogo} alt="Photoshop logo" style={{width: "130px", marginTop: "30px"}}/>
+                                    <h3 style={{marginTop:"40px"}}>Adobe Photoshop</h3>
                                 </div>
                             
                             </Carousel>
